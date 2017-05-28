6776adf7bb8dc03499dc22bbb6c0f65aec3b1d38
MagicBowen <e.bowen.wang@icloud.com>
2017-05-28 17:18:04 +0800

    order two streams

--- python/rxlog/rxlog.py
+++ python/rxlog/rxlog.py
@@ -26,3 +26,52 @@ stream = Observable.from_(generate_csv_stream('issue.csv'))                     
                    .filter(lambda item: item.type is not None)              \
                    .map(lambda item: {'type' : item.type, 'time' : item.time})                                    \
                    .subscribe(lambda item: print('receive {}'.format(item)))    
+
+
+
+class StreamWrapper:
+  def __init__(self, value, sid = 0):
+    self.value = value
+    self.property = {'id' : sid}
+
+  def set(self, name, value):
+    self.property[name] = value
+
+  def get(self, name):
+    return self.property[name]
+
+  def value(self):
+    return self.value
+
+  def __str__(self):
+    return 'value: {}, property: {}'.format(self.value, self.property)
+
+# bucket = {'s' : [[],[]], 'value' : None}    
+
+def order(acc, item):
+  acc['s'][item['id']].append(item['value'])
+
+  if len(acc['s'][0]) == 0 or len(acc['s'][1]) == 0:
+    return {'s' : acc['s'], 'value' : None}
+
+  result = []
+
+  while len(acc['s'][0]) != 0 and len(acc['s'][1]) != 0:
+    if acc['s'][0][0] < acc['s'][1][0]:
+      result.append(acc['s'][0][0])
+      acc['s'][0].pop(0)
+    else:
+      result.append(acc['s'][1][0])
+      acc['s'][1].pop(0)  
+
+  return  {'s' : acc['s'], 'value' : Observable.from_(result)}
+
+
+
+s1 = Observable.from_([1, 3, 4, 6, 9, 10, 11, 13, 14]).map(lambda item: {'id' : 0, 'value' : item})
+s2 = Observable.from_([0, 2, 5, 7, 8, 12, 15]).map(lambda item: {'id' : 1, 'value' : item})
+
+s1.merge(s2).scan(order, {'s' : [[],[]], 'value' : None}).filter(lambda item: item['value'] is not None).flat_map(lambda item: item['value']).subscribe(lambda item: print(item))
+
+
+
