f4d6301b67ce00c1cf15dd0edda0d0fe3e7775c8
magicbowen <e.bowen.wang@icloud.com>
2018-06-15 17:54:28 +0800

    add more cases

--- java/scripting-node/src/main/java/Main.java
+++ java/scripting-node/src/main/java/Main.java
@@ -92,23 +92,18 @@ public class Main {
         nodeJS.release();
     }
 
-    private static void testModifyThis() {
+    private static void testAddFunction() {
         final NodeJS nodeJS = NodeJS.createNodeJS();
-        final V8Object doSomething = nodeJS.require(new File("./js/this.js"));
+        final V8Object doSomething = nodeJS.require(new File("./js/java-callback.js"));
 
-//        JavaCallback result = new JavaCallback() {
-//
-//            public Object invoke(V8Object receiver, V8Array parameters) {
-//                System.out.println("parameter length is " + parameters.length());
-//                V8Object resultObj = parameters.getObject(0);
-//                int result = resultObj.getInteger("result");
-//                System.out.println("result is " + result);
-//                resultObj.release();
-//                return null;
-//            }
-//        };
-//
-//        V8Object callback = new V8Function(nodeJS.getRuntime(), result);
+        JavaCallback callback = new JavaCallback() {
+
+            public Object invoke(V8Object receiver, V8Array parameters) {
+                return "5";
+            }
+        };
+
+        nodeJS.getRuntime().registerJavaMethod(callback, "javaFunction");
 
         doSomething.executeJSFunction("doSomething");
 
@@ -120,6 +115,41 @@ public class Main {
         nodeJS.release();
     }
 
+    private static void testAddGlobalObject() {
+        final NodeJS nodeJS = NodeJS.createNodeJS();
+
+        Global g = new Global(5);
+
+        JavaCallback getValue = new JavaCallbackImpl(g) {
+
+            public Object invoke(V8Object receiver, V8Array parameters) {
+                return g.getValue();
+            }
+        };
+
+        JavaCallback increase = new JavaCallbackImpl(g) {
+
+            public Object invoke(V8Object receiver, V8Array parameters) {
+                g.increase();
+                return null;
+            }
+        };
+
+        nodeJS.getRuntime().registerJavaMethod(getValue, "getValueInJava");
+        nodeJS.getRuntime().registerJavaMethod(increase, "increaseInJava");
+
+
+        File nodeScript = new File("./js/" + "use-global.js");
+
+        nodeJS.exec(nodeScript);
+
+        while(nodeJS.isRunning()) {
+            nodeJS.handleMessage();
+        }
+
+        nodeJS.release();
+    }
+
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         invokeJsFile("features");
@@ -127,5 +157,7 @@ public class Main {
         testNodeJsModuleExport();
         testHttpModuleCallback();
         testRequestModuleCallback();
+        testAddFunction();
+        testAddGlobalObject();
     }
 }
